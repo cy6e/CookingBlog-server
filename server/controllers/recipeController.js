d7dc839627e84ac938d7e0fb6a122353960a85b1
cy6e <97270008+cy6e@users.noreply.github.com>
2023-09-03 21:09:04 +0530

    Update recipeController.js

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -32,6 +32,7 @@ exports.about = async(req, res) => {
   const infoSubmitObj = req.flash('infoSubmit');
   res.render('about', { title: 'CookBook - About', infoErrorsObj, infoSubmitObj  } );
 }
+
 /**
  * GET /categories
  * Categories 
@@ -185,8 +186,8 @@ exports.submitRecipeOnPost = async(req, res) => {
 
 // Delete Recipe
 // async function deleteRecipe(){
-//  try {
-//    await Recipe.deleteOne({ name: 'New Chocolate Cake' });
+//   try {
+//     await Recipe.deleteOne({ name: 'New Recipe From Form' });
 //   } catch (error) {
 //     console.log(error);
 //   }
@@ -207,3 +208,77 @@ exports.submitRecipeOnPost = async(req, res) => {
 // updateRecipe();
 
 
+/**
+ * Dummy Data Example 
+*/
+
+// async function insertDymmyCategoryData(){
+//   try {
+//     await Category.insertMany([
+//       {
+//         "name": "Thai",
+//         "image": "thai-food.jpg"
+//       },
+//       {
+//         "name": "American",
+//         "image": "american-food.jpg"
+//       }, 
+//       {
+//         "name": "Chinese",
+//         "image": "chinese-food.jpg"
+//       },
+//       {
+//         "name": "Mexican",
+//         "image": "mexican-food.jpg"
+//       }, 
+//       {
+//         "name": "Indian",
+//         "image": "indian-food.jpg"
+//       },
+//       {
+//         "name": "Spanish",
+//         "image": "spanish-food.jpg"
+//       }
+//     ]);
+//   } catch (error) {
+//     console.log('err', + error)
+//   }
+// }
+
+// insertDymmyCategoryData();
+
+
+// async function insertDymmyRecipeData(){
+//   try {
+//     await Recipe.insertMany([
+//       { 
+//         "name": "Recipe Name Goes Here",
+//         "description": `Recipe Description Goes Here`,
+//         "email": "recipeemail@raddy.co.uk",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "American", 
+//         "image": "southern-friend-chicken.jpg"
+//       },
+//       { 
+//         "name": "Recipe Name Goes Here",
+//         "description": `Recipe Description Goes Here`,
+//         "email": "recipeemail@raddy.co.uk",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "American", 
+//         "image": "southern-friend-chicken.jpg"
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log('err', + error)
+//   }
+// }
+
+// insertDymmyRecipeData();
